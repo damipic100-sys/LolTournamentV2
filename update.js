b7a98dce2331860199f07acc90f9ca6c66e1f961
damipic100-sys <damipic100@gmail.com>
2026-01-29 23:08:26 -0300

    Auto-update: jue. 29/01/2026 23:08:26,49

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import fs from "fs";
 
-const API_KEY = "RGAPI-4cc5bca4-32c5-4cea-af5a-d5b2e60b093a";
+const API_KEY = "RGAPI-a6fd9f28-52a1-4752-817c-5b620ff80b2c";
 const REGION = "americas"; // account-v1
 const PLATFORM = "la2";    // la2 / la1 / br1, etc
 
