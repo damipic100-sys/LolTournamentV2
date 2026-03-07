9330f79c5e3154d373401d163354d87b3c700387
damipic100-sys <damipic100@gmail.com>
2026-03-07 20:14:58 -0300

    Auto-update: sáb. 07/03/2026 20:14:58,19

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import fs from "fs";
 
-const API_KEY = "RGAPI-a9186e68-50a9-4e34-a58e-911c7313c351";
+const API_KEY = "RGAPI-88a483a7-94c0-4476-a67f-8aa13128f11a";
 const REGION = "americas"; // account-v1
 const PLATFORM = "la2";    // la2 / la1 / br1, etc
 
